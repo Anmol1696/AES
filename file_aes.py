26aca87ae6cb62a75095a7b82a5b2312e7b0b396
siddharth <siddharth140010017@github.com>
2015-06-19 08:16:30 +0530

    little change

---
+++ file_aes.py
@@ -0,0 +1,26 @@
+import main as aes
+
+def main(a,b,step):
+	f = open(a,'r')
+	r = f.readlines()
+	f.close()
+	o = ''
+	for x in r:
+		o += x
+	if(step == 'e'):
+		o = aes.Main_Encrypt(o,'')
+	else:
+		o = aes.Main_Decrypt(o,'')
+	w = open(b,'w')
+	w.write(o)
+	w.close()
+
+if __name__ == '__main__':
+	step = raw_input('Enter the e or d ->')
+	source_file = raw_input('Enter the location of the source file')
+	target_file = raw_input('Enter the location of the target file')
+	main(source_file,target_file,step)
+
+
+
+
