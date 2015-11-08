c5067685313176dbd6bfe10dd97057a0e9bf2f4e
Anmol1696 <anmol1696@gmail.com>
2015-11-09 00:17:53 +0530

    Cleaning the code for the main Rounds

--- src/aes.py
+++ src/aes.py
@@ -1,7 +1,7 @@
 from tables import *
 
 class AesClass:
-	
+
 	def __init__(self,a):
 		self.key = []
 		self.do = a
@@ -9,7 +9,7 @@ class AesClass:
 		self.decrypt = ''
 		self.number = 0
 		self.node = []
-	
+
 	# Plain text to node
 	def text_node(self,inp):
 		noder = len(inp)/16 + 1
@@ -23,11 +23,11 @@ class AesClass:
 				temp[v/4][v%4] = q
 			box[u] = temp
 		self.node = box
-		
+
 	# Cipher text to node
 	def cipher_node(self,a):
 		noder = len(a)/22
-		self.number = noder 
+		self.number = noder
 		box = [0 for x in range(noder)]
 		for o in range(0,len(a),22):
 			Bin = []
@@ -48,7 +48,7 @@ class AesClass:
 				temp[u/4][u%4] = fin[u]
 			box[o/22] = temp
 		self.node = box
-		
+
 	# key to node
 	def key_node(self,a):
 		key_pad = ')!(@*#&$%^<+*/>?'
@@ -56,105 +56,106 @@ class AesClass:
 		keytemp = [0 for x in range(11)]
 		keytemp[0] = [[(ord(a[i])^ord(key_pad[i])) for i in range(0+x,4+x)]for x in range(0,16,4)]
 		self.key = keytemp
-	
+
 	# making of the Round keys
 	def Roundkeys(self):
-		
+
 		for rnd  in range(10) :
 			coltemp = [self.key[rnd][0][x] for x in range(4)]
 			colcur = [self.key[rnd][3][x] for x in range(4)]
 			(colcur[0],colcur[1],colcur[2],colcur[3]) = (colcur[1],colcur[2],colcur[3],colcur[0])
-			
+
 			for x in range(4):
 				r = (colcur[x]>>4) & 0x0F
 				c = (colcur[x]>>0) & 0x0F
 				colcur[x] = S_box[r][c]
 				colcur[x] = (coltemp[x]^colcur[x]^Rcon[rnd][x])
 			tempkey = [[0 for x in range(4)]for x in range(4)]
-			tempkey[0] = colcur 
-			self.key[rnd+1] = tempkey 
+			tempkey[0] = colcur
+			self.key[rnd+1] = tempkey
 			for i in range(1,4):
 				for j in range(4):
 					coltemp[j] = self.key[rnd][i][j]
 					colcur[j] = self.key[rnd+1][i-1][j]
 					colcur[j] = coltemp[j]^colcur[j]
 					self.key[rnd+1][i][j] = colcur[j]
-	
-	def reverse_keys(self):
+
+	def _reverse_keys(self):
 		(self.key[0],self.key[1],self.key[2],self.key[3],self.key[4],self.key[5],self.key[6],self.key[7],self.key[8],self.key[9],self.key[10])\
 			= (self.key[10],self.key[9],self.key[8],self.key[7],self.key[6],self.key[5],self.key[4],self.key[3],self.key[2],self.key[1],self.key[0])
-	
+
+	def _sbox_substitution(self, repetation, inverse = False):
+		for i in range(4):
+			for j in range(4):
+				r = (self.node[repetation][i][j]>>4)&0x0F
+				c = (self.node[repetation][i][j]>>0)&0x0F
+				if inverse: self.node[repetation][i][j] = inS_box[r][c]
+				else: self.node[repetation][i][j] = S_box[r][c]
+
+	def _shift_rows(self, repetation, inverse = False):
+		if inverse: num = 3
+		else: num = 1
+		for i in range(1,4):
+			for u in range(i*num):
+				for j in range(3):
+					(self.node[repetation][j][i],self.node[repetation][j+1][i]) = (self.node[repetation][j+1][i],self.node[repetation][j][i])
+
+	def _mix_columns(self, repetation, inverse = False):
+		for j in range(4):
+			coltemp = [self.node[repetation][j][i] for i in range(4)]
+			if inverse:
+				col = [table_14[coltemp[0]]^table_11[coltemp[1]]^table_13[coltemp[2]]^table_9[coltemp[3]],\
+					table_9[coltemp[0]]^table_14[coltemp[1]]^table_11[coltemp[2]]^table_13[coltemp[3]],\
+					table_13[coltemp[0]]^table_9[coltemp[1]]^table_14[coltemp[2]]^table_11[coltemp[3]],\
+					table_11[coltemp[0]]^table_13[coltemp[1]]^table_9[coltemp[2]]^table_14[coltemp[3]]]
+
+			else:
+				col = [table_2[coltemp[0]]^table_3[coltemp[1]]^coltemp[2]^coltemp[3],\
+				coltemp[0]^table_2[coltemp[1]]^table_3[coltemp[2]]^coltemp[3],\
+				coltemp[0]^coltemp[1]^table_2[coltemp[2]]^table_3[coltemp[3]],\
+				table_3[coltemp[0]]^coltemp[1]^coltemp[2]^table_2[coltemp[3]]]
+			self.node[repetation][j] = col
+
+	def _add_round_keys(self, repetation, num_round):
+		for j in range(4):
+			for i in range(4):
+				self.node[repetation][j][i] = self.node[repetation][j][i]^self.key[num_round][j][i]
+
 	def MainRounds(self):
 		# Tables
 		if self.do == '0':
 			for rep in range(self.number):
 				for rnd in range(11):
 					if rnd > 0:
-						# Sbox substitution
-						for i in range(4):
-							for j in range(4):
-								r = (self.node[rep][i][j]>>4)&0x0F
-								c = (self.node[rep][i][j]>>0)&0x0F
-								self.node[rep][i][j] = S_box[r][c]
-						
-						# Shift rows
-						for i in range(1,4):
-							for u in range(i):
-								for j in range(3):
-									(self.node[rep][j][i],self.node[rep][j+1][i]) = (self.node[rep][j+1][i],self.node[rep][j][i])
-						
+						self._sbox_substitution(rep)
+
+						self._shift_rows(rep)
+
 						if rnd < 10:
-							# Mix Columns
-							for j in range(4):
-								coltemp = [self.node[rep][j][i] for i in range(4)]
-								col = [table_2[coltemp[0]]^table_3[coltemp[1]]^coltemp[2]^coltemp[3],\
-									coltemp[0]^table_2[coltemp[1]]^table_3[coltemp[2]]^coltemp[3],\
-									coltemp[0]^coltemp[1]^table_2[coltemp[2]]^table_3[coltemp[3]],\
-									table_3[coltemp[0]]^coltemp[1]^coltemp[2]^table_2[coltemp[3]]]
-								self.node[rep][j] = col
-					
-					# Add Round keys
-					for j in range(4):
-						for i in range(4):
-							self.node[rep][j][i] = self.node[rep][j][i]^self.key[rnd][j][i]
-		
+							self._mix_columns(rep)
+
+					self._add_round_keys(rep, rnd)
+
 		else :
-			self.reverse_keys()
+			self._reverse_keys()
 			for rep in range(self.number):
 				for rnd in range(11):
 					if rnd > 0:
-						# Inverse Shift rows
-						for i in range(1,4):
-							for u in range(i*3):
-								for j in range(3):
-									(self.node[rep][j][i],self.node[rep][j+1][i]) = (self.node[rep][j+1][i],self.node[rep][j][i])
-						# Inverse Sbox substitution
-						for i in range(4):
-							for j in range(4):
-								r = (self.node[rep][i][j]>>4)&0x0F
-								c = (self.node[rep][i][j]>>0)&0x0F
-								self.node[rep][i][j] = inS_box[r][c]
-					# Add Round keys
-					for j in range(4):
-						for i in range(4):
-							self.node[rep][j][i] = self.node[rep][j][i]^self.key[rnd][j][i]
-					
+						self._shift_rows(rep, True)
+
+						self._sbox_substitution(rep, True)
+
+					self._add_round_keys(rep, rnd)
+
 					if rnd<10 and rnd>0:
-						# Mix Columns
-						for j in range(4):
-							coltemp = [self.node[rep][j][i] for i in range(4)]
-							col = [table_14[coltemp[0]]^table_11[coltemp[1]]^table_13[coltemp[2]]^table_9[coltemp[3]],\
-								table_9[coltemp[0]]^table_14[coltemp[1]]^table_11[coltemp[2]]^table_13[coltemp[3]],\
-								table_13[coltemp[0]]^table_9[coltemp[1]]^table_14[coltemp[2]]^table_11[coltemp[3]],\
-								table_11[coltemp[0]]^table_13[coltemp[1]]^table_9[coltemp[2]]^table_14[coltemp[3]]]
-							self.node[rep][j] = col
-					
+						self._mix_columns(rep, True)
+
 					if rnd == 10:
 						for i in range(4):
 							for j in range(4):
 								a = chr(self.node[rep][i][j])
 								self.decrypt += a
-	
+
 	def binary_6(self):
 		for rep in range(self.number):
 			Bin = []
@@ -165,15 +166,15 @@ class AesClass:
 						if ((self.node[rep][x][y]>>i)%2==1) : q=1
 						else : q=0
 						Bin.append(q)
-			for x in range(4): Bin.append(0) 
+			for x in range(4): Bin.append(0)
 			for i in range(0,132,6):
 				a = 0
 				for q in range(6): a += (pow(2,5-q)*Bin[q+i])
 				rare[i/6] = chr(a+32)
-			for u in range(22): self.encrypt += rare[u]  
-	
+			for u in range(22): self.encrypt += rare[u]
+
 	def Decrypted(self):
 		return self.decrypt
-	
+
 	def Encrypted(self):
 		return self.encrypt
